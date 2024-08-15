2d2cba3f9a17560c522e2ea2d97a5c0aef85e4e4
MDShihab235 <msub111244@gmail.com>
2024-08-15 20:17:37 +0600

    ai-saas-podcast-platform

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://sharing-chamois-75.clerk.accounts.dev
+
+const authConfig = {
+  providers: [
+    {
+      domain: "https://sharing-chamois-75.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
